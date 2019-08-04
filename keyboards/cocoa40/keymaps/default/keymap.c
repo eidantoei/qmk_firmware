edfa2537047f009d2c689d60f283abe3d7b19c59
kssk <kusaker@gmail.com>
2019-08-04 21:41:05 +0900

    Swap ESC and TAB 2

--- keyboards/cocoa40/keymaps/default/keymap.c
+++ keyboards/cocoa40/keymaps/default/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LALT,  KC_LGUI, KC_NUMS, KC_SYMS, KC_RGUI, KC_RALT
   ),
   [_NUMS] = LAYOUT(
-    KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSLASH,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSLASH,
     _______, _______, _______, _______, _______, _______
   ),
   [_SYMS] = LAYOUT(
