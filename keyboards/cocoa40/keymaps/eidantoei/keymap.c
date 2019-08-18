6969b42c95fa9793b70d86f192bb32dba2b80288
kssk <kusaker@gmail.com>
2019-08-18 21:34:04 +0900

    Swap TILDE and BACKQUOTE

--- keyboards/cocoa40/keymaps/eidantoei/keymap.c
+++ keyboards/cocoa40/keymaps/eidantoei/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______
   ),
   [_SYMS] = LAYOUT(
-    KC_TILDE, KC_EXCLAIM, KC_AT, KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_MINUS, KC_PLUS, KC_PIPE,
+    KC_GRAVE, KC_EXCLAIM, KC_AT, KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_MINUS, KC_PLUS, KC_PIPE,
     _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______
